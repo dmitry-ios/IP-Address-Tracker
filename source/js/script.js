df4b90192ff6a03f18a143d83a624602111c2720
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-11-14 19:57:50 +0300

    Стилизует мобильную версию сайта

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+const leafletMap = L.map('mapid').setView([51.505, -0.09], 13)
+
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG1pdHJ5YiIsImEiOiJja2hld293cW8wYThxMnNwbDkxdWhwNjZlIn0.EI5vC4TroTo2dOQN-deCQQ',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+  },
+).addTo(leafletMap)
